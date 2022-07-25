a4e09eac54b51662365c034174cd9049d621153d
Gabrielle <gabriellemarie@knights.ucf.edu>
2022-07-25 16:30:05 -0700

    initial commit - added profile && posts model and controller

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://picsum.photos/200" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> posts</div>
+                <div class="pe-5"><strong>23k</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
+            </div>
+
+            <div class="pt-4 fw-bold">{{$user->profile->title}}</div>
+            <div>{{$user->profile->description}}</div>
+            <div><a href="#">{{$user->profile->url}}</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://picsum.photos/200" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://picsum.photos/200" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://picsum.photos/200" class="w-100">
+        </div>
+    </div>
+</div>
+@endsection
